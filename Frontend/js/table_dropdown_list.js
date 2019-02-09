ba78daacf830a96e1373182f808abc10d5db7170
linda4321 <magur.polina@gmail.com>
2019-02-09 22:21:31 +0200

    comment button added

--- Frontend/js/table_dropdown_list.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $(".dropdown-menu li a").click(function(){
-
-        $(".dropdown-btn-val .order-header").text($(this).text());
-        $(".dropdown-btn-val .order-header").val($(this).text());
-    });
-});
