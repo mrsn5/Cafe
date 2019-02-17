56ae40a83e55d9a25fbc542cd6a3989baa39e804
San Nguyen <mr.sn5.kma@gmail.com>
2019-02-17 22:55:47 +0200

    backend

---
+++ Backend/db/dbhelper.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class DBHelper {
+    private static $servername = "localhost";
+    private static $username = "root";
+    private static $password = "root";
+    private static $port = 8889;
+    private static $myDB = "cafe";
+    private static $conn = null;
+
+    static function connect() {
+        try{
+            self::$conn = new PDO("mysql:host=".self::$servername.
+                "; dbname=".self::$myDB.
+                "; port=".self::$port,
+                self::$username,
+                self::$password);
+            // set the PDO error mode to exception
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$conn->exec("SET NAMES 'utf8'");
+
+        }catch(Exception $e){
+            echo "Error: " . $e->getMessage();
+            exit;
+        }
+        return self::$conn;
+    }
+
+    public static function disconnect() {
+        self::$conn = null;
+    }
+}
+
+?>
