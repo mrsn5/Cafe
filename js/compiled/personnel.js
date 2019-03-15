053ea83d073320dfb3f5a93bdaee45bb84d37b7d
San Nguyen <mr.sn5.kma@gmail.com>
2019-03-15 14:47:06 +0200

    pers

--- js/compiled/personnel.js
+++ js/compiled/personnel.js
@@ -24,58 +24,45 @@ $("#search-button").click(function () {
 
 function get_personnel(position, name) {
     $personnel_table.html("");
-
-    if (position && name) {
-        url = '../Backend/personnel.php?type=select&position=' + position + '&name=' + name;
-    } else if (position) {
-        url = '../Backend/personnel.php?type=select&position=' + position;
-    } else if (name) {
-        url = '../Backend/personnel.php?type=select&name=' + name;
-    } else {
-        url = '../Backend/personnel.php?type=select';
-    }
-
-
-    console.log(url);
-
-    fetch(url)
-        .then(data=>{return data.json();})
-        .then(function (res) {
-            console.log(res);
+    $.ajax({
+        url: 'http://localhost/Cafe/wordpress/wp-admin/admin-ajax.php',
+        type: 'POST',
+        data: {
+            action: 'personnel_select',
+            position: position,
+            name: name
+        },
+        success: function (res) {
+            res = JSON.parse(res);
             res.forEach(function (p) {
                 var $node = $(personnel(p));
                 $personnel_table.append($node);
             });
-        });
+        }
+    });
 }
 
 $("#save-personnel").click(function () {
-    var tab_num = $("#tab_num").val();
-    var position = $("#position-add").val();
-    var name = $("#first_name").val();
-    var surname = $("#surname").val();
-    var fathername = $("#father_name").val();
-    var birthdate = $("#birth_date").val();
-    var gender = $("#is_male:checked").val() || $("#is_female:checked").val();//? "Ч" : "Ж";
-    var address = $("#address").val();
-    var telephone = $("#tel_num").val();
-
-    console.log(tab_num, position, name, surname, fathername, birthdate, gender, address, telephone);
-    fetch('../wp-content/themes/cafe_theme/Backend/personnel.php?type=add'
-                + '&first_name=' + name
-                + '&surname=' + surname
-                + '&father_name=' + fathername
-                + '&tab_num=' + tab_num
-                + '&position=' + position
-                + '&birth_date=' + birthdate
-                + '&gender=' + gender
-                + '&address=' + address
-                + '&tel_num=' + telephone
-                + '&salary=6000')
-        .then(data=>{return data.text();})
-        .then(function (res) {
-                console.log(res);
-            });
+    $.ajax({
+        url: 'http://localhost/Cafe/wordpress/wp-admin/admin-ajax.php',
+        type: 'POST',
+        data: {
+            action: 'personnel_add',
+            tab_num: $("#tab_num").val(),
+            position: $("#position-add").val(),
+            name: $("#first_name").val(),
+            surname: $("#surname").val(),
+            fathername: $("#father_name").val() == '' ? 'NULL' : $("#father_name").val(),
+            birthdate: $("#birth_date").val(),
+            gender: $("#is_male:checked").val() || $("#is_female:checked").val(),
+            address: $("#address").val(),
+            telephone: $("#tel_num").val(),
+            salary: 5000
+        },
+        success: function (res) {
+            console.log(res);
+        }
+    });
 });
 },{"ejs":3}],2:[function(require,module,exports){
 
