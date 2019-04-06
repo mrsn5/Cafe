4b70a0c16d85ea135f573daffc10d4ce72b4a1de
San Nguyen <mr.sn5.kma@gmail.com>
2019-04-06 12:59:07 +0300

    tab num added to wp_user

--- Backend/users_roles.php
+++ Backend/users_roles.php
@@ -12,13 +12,14 @@ remove_role( 'editor' );
 remove_role( 'author' );
 remove_role( 'contributor' );
 remove_role( 'subscriber' );
+remove_role( 'worker' );
 add_role( 'waiter', ('Офіціант' ), array( ) );
 add_role( 'cook', ('Кухар' ), array( ) );
 add_role( 'bookkeeper', ('Бухгалтер' ), array( ) );
 add_role( 'barman', ('Бармен' ), array( ) );
 add_role( 'owner', ('Власник' ), array( ) );
 add_role( 'chef', ('Шуф-кухар' ), array( ) );
-add_role( 'worker', ('Робітник' ), array( ) );
+
 
 
 add_action( 'wp_ajax_get_user_role', 'get_user_role' );
@@ -37,6 +38,79 @@ function get_user_role(){
 //
 //        }
     }
+}
+
+
+
+add_action( 'user_new_form', 'crf_admin_registration_form' );
+function crf_admin_registration_form( $operation ) {
+    if ( 'add-new-user' !== $operation ) {
+        // $operation may also be 'add-existing-user'
+        return;
+    }
+
+    $tab_num = ! empty( $_POST['tab_num'] ) ? $_POST['tab_num'] : '';
+
+    ?>
+
+
+    <table class="form-table">
+        <tr>
+            <th><label for="tab_num"><?php esc_html_e( 'Табельний номер', 'crf' ); ?></label> <span style="color: red" class="description"><?php esc_html_e( '*', 'crf' ); ?></span></th>
+            <td>
+                <input type="text"
+                       size="3"
+                       id="tab_num"
+                       name="tab_num"
+                       value="<?php echo esc_attr( $tab_num ); ?>"
+                       class="regular-text"
+                />
+            </td>
+        </tr>
+    </table>
+    <?php
+}
+
+
+
+add_action( 'user_profile_update_errors', 'crf_user_profile_update_errors', 10, 3 );
+function crf_user_profile_update_errors( $errors, $update, $user ) {
+    if ( $update ) {
+        return;
+    }
+
+
+    if ( empty( $_POST['tab_num'] ) ) {
+        $errors->add( 'tab_num_error', __( '<strong>Помилка</strong>: Будь-ласка введіть табельний номер.', 'crf' ) );
+    }
+
+
+    global $wpdb;
+    if ( ! empty( $_POST['tab_num'] ) && count((array)(($wpdb->get_results( "SELECT * FROM workers WHERE tab_num='$_POST[tab_num]'")))) == 0) {
+        $errors->add( 'tab_num_error', __( '<strong>Помилка</strong>: Табельний номер не існує.', 'crf' ) );
+    }
+}
+
+
+add_action( 'edit_user_created_user', 'crf_user_register' );
+function crf_user_register( $user_id ) {
+    if ( ! empty( $_POST['tab_num'] ) ) {
+        update_user_meta( $user_id, 'tab_num', $_POST['tab_num']);
+    }
+}
+
+add_action( 'show_user_profile', 'crf_show_extra_profile_fields' );
+add_action( 'edit_user_profile', 'crf_show_extra_profile_fields' );
+
+function crf_show_extra_profile_fields( $user ) {
+    ?>
+    <table class="form-table">
+        <tr>
+            <th><label for="tab_num"><?php esc_html_e( 'Табельний номер', 'crf' ); ?></label></th>
+            <td><?php echo esc_html( get_the_author_meta( 'tab_num',  $user->ID ) ); ?></td>
+        </tr>
+    </table>
+    <?php
 }
 
 
--- home.php
+++ home.php
@@ -1,6 +1,9 @@
 <?php
 
 define("PATH", get_template_directory_uri());
+$user = wp_get_current_user();
+$role = ((array) $user->roles)[0];
+
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +20,7 @@ define("PATH", get_template_directory_uri());
 <div class="header">
 
 </div>
+<?php echo 1 == count((array)(($wpdb->get_results( "SELECT * FROM workers WHERE tab_num='506'")))) ?>
 
 <!-- Tab container -->
 <div class="tab-container">
@@ -24,15 +28,24 @@ define("PATH", get_template_directory_uri());
         <li style="background-image: linear-gradient(0deg, #50c3ff, #046aec);">
             <a href="menu"><img src="<?php echo PATH?>/images/menu.svg" alt="Menu"/></a>
         </li>
+
+        <?php if($role != 'bookkeeper') { ?>
         <li style="background-image: linear-gradient(180deg, #ffd53a, #ea7b00);">
             <a href="orders"><img src="<?php echo PATH?>/images/serving-dish.svg" alt="Orders"/></a>
         </li>
+        <?php } ?>
+
+        <?php if($role != 'waiter' && $role != 'cook') { ?>
         <li style="background-image: linear-gradient(150deg, #94ffaf, #00bf52);">
             <a href="storage"><img src="<?php echo PATH?>/images/open-box.svg" alt="Storage"/></a>
         </li>
+        <?php } ?>
+
         <li style="background-image: linear-gradient(20deg, #e134b7, #ffd2f6);">
             <a href="deliveries"><img src="<?php echo PATH?>/images/truck.svg" alt="Deliveries"/></a>
         </li>
+
+
         <li style="background-image: linear-gradient(150deg, rgb(229,173,255), #9532b5);">
             <a href="personnel"><img src="<?php echo PATH?>/images/waiter.svg" alt="Personnel"/></a>
         </li>
